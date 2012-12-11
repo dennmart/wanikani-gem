e28c7d08e51a41c30235f5422524067fb8b901da
Dennis Martinez <dennis@dennmart.com>
2012-12-10 20:48:49 -0800

    Added basic SRS Distribution methods

---
+++ lib/wanikani/srs.rb
@@ -0,0 +1,15 @@
+module Wanikani
+  class SRS
+    ITEM_TYPES = %w(apprentice guru master enlighten burned all)
+
+    def self.distribution(item_type = "all")
+      raise ArgumentError, "Please use a valid SRS type (or none for all types)" if !ITEM_TYPES.include?(item_type)
+
+      api_response = Wanikani.api_response("srs-distribution")
+      srs_distribution = api_response["requested_information"]
+
+      return srs_distribution if item_type == "all"
+      return srs_distribution[item_type]
+    end
+  end
+end
