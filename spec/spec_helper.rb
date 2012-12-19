bc9e6252248d95cc70f09646a9978465d2e2bf12
Dennis Martinez <dennis@dennmart.com>
2012-12-18 20:31:16 -0800

    Remove some unnecessary Rspec options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,6 @@ require 'fakeweb'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
   config.order = 'random'
   config.before(:each) { Wanikani.api_key = "WANIKANI-API-KEY" }
 end
