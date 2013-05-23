dd3a92ee8878e360a7fddc963f331dde123afc23
Dennis Martinez <dennis@dennmart.com>
2013-05-22 20:17:49 -0700

    Dynamically create some similar methods to avoid duplication

--- lib/wanikani/level.rb
+++ lib/wanikani/level.rb
@@ -10,33 +10,14 @@ module Wanikani
       return api_response["requested_information"].merge(current_level)
     end
 
-    # Gets the radicals for the specified levels.
-    #
-    # @param [Integer, Array] levels returns the list of radicals for one or more levels
-    # @return [Array] Hashes with the radicals for the specified level(s).
-    def self.radicals(levels = nil)
-      levels = levels.join(',') if levels.is_a?(Array)
-      api_response = Wanikani.api_response("radicals", levels)
-      return api_response["requested_information"]
-    end
+    private
 
-    # Gets the Kanji for the specified levels.
-    #
-    # @param [Integer, Array] levels returns the list of Kanji for one or more levels
-    # @return [Array] Hashes with the Kanji for the specified level(s).
-    def self.kanji(levels = nil)
-      levels = levels.join(',') if levels.is_a?(Array)
-      api_response = Wanikani.api_response("kanji", levels)
-      return api_response["requested_information"]
-    end
+    def self.method_missing(name, *args)
+      super unless [:radicals, :kanji, :vocabulary].include?(name)
 
-    # Gets the vocabulary for the specified levels.
-    #
-    # @param [Integer, Array] levels returns the list of vocabulary for one or more levels
-    # @return [Array] Hashes with the vocabulary for the specified level(s).
-    def self.vocabulary(levels = nil)
+      levels = args.push
       levels = levels.join(',') if levels.is_a?(Array)
-      api_response = Wanikani.api_response("vocabulary", levels)
+      api_response = Wanikani.api_response(name.to_s, levels)
       return api_response["requested_information"]
     end
   end
