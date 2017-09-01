3b8f5c4c7e660b89584a45933aef6975720629fb
Dennis Martinez <dennis@dennmart.com>
2017-09-01 15:51:59 +0900

    Add documentation to Wanikani::Client

--- lib/wanikani/client.rb
+++ lib/wanikani/client.rb
@@ -20,6 +20,11 @@ module Wanikani
 
     attr_accessor :api_key, :api_version
 
+    # Initialize a client which will be used to communicate with WaniKani.
+    #
+    # @param options [Hash] the API key (required) and API version (optional)
+    #   used to communicate with the WaniKani API.
+    # @return [Wanikani::Client] an instance of Wanikani::Client.
     def initialize(options = {})
       raise ArgumentError, "You must specify a WaniKani API key before querying the API." if options[:api_key].nil? || options[:api_key].empty?
       raise ArgumentError, "API version should be one of the following: #{Wanikani::VALID_API_VERSIONS.join(', ')}." unless Wanikani::VALID_API_VERSIONS.include?(options[:api_version]) || options[:api_version].nil?
@@ -28,6 +33,10 @@ module Wanikani
       @api_version = options[:api_version] ||= Wanikani::DEFAULT_API_VERSION
     end
 
+    # Verifies if the client's API key is valid by checking WaniKani's API.
+    #
+    # @param api_key [String] the API key to validate in WaniKani.
+    # @return [Boolean] whether the API key is valid.
     def valid_api_key?(api_key = nil)
       api_key ||= @api_key
       return false if api_key.empty?
@@ -38,15 +47,23 @@ module Wanikani
       return true
     end
 
+    # Verifies if the specified API key is valid by checking WaniKani's API.
+    #
+    # @param api_key [String] the API key to validate in WaniKani.
+    # @return [Boolean] whether the API key is valid.
     def self.valid_api_key?(api_key = nil)
       raise ArgumentError, "You must specify a WaniKani API key before querying the API." if api_key.nil? || api_key.empty?
 
-      client = Wanikani::Client.new(api_key: api_key)
-      return client.valid_api_key?
+      @client = Wanikani::Client.new(api_key: api_key)
+      return @client.valid_api_key?
     end
 
     private
 
+    # Sets up the HTTP client for communicating with the WaniKani API.
+    #
+    # @return [Faraday::Connection] the HTTP client to communicate with the
+    #   WaniKani API.
     def client
       Faraday.new(url: Wanikani::API_ENDPOINT) do |conn|
         conn.response :json, :content_type => /\bjson$/
@@ -54,6 +71,11 @@ module Wanikani
       end
     end
 
+    # Contacts the WaniKani API and returns the data specified.
+    #
+    # @param resource [String] the resource to access.
+    # @param optional_arg [String] optional arguments for the specified resource.
+    # @return [Hash] the parsed API response.
     def api_response(resource, optional_arg = nil)
       raise ArgumentError, "You must define a resource to query WaniKani" if resource.nil? || resource.empty?
 
@@ -70,6 +92,9 @@ module Wanikani
       end
     end
 
+    # Handles exceptions according to the API response.
+    #
+    # @param response [Hash] the parsed API response from WaniKani's API.
     def raise_exception(response)
       raise Wanikani::InvalidKey, "The API key used for this request is invalid." and return if response.status == 401
 
