6df57a2d72a5c5bc36b72c2b345021f6d0b34a18
Dennis Martinez <dennis@dennmart.com>
2017-09-01 16:10:10 +0900

    Update Wanikani::Level to be explicit instead of using method_missing

--- lib/wanikani/level.rb
+++ lib/wanikani/level.rb
@@ -3,28 +3,58 @@ module Wanikani
   module Level
     # Gets the user's current level progression (radicals and Kanji).
     #
-    # @return [Hash] Progress and total of radicals and Kanji for the user's current level.
+    # @return [Hash] progress and total of radicals and Kanji for the user's current level.
     def level_progression
       response = api_response("level-progression")
       current_level = { "current_level" => response["user_information"]["level"] }
       return response["requested_information"].merge(current_level)
     end
 
+    # Gets the user's full list of radicals and stats.
+    #
+    # @param levels [Integer, Array<Integer>] a specific level or array of
+    #   levels to fetch items for.
+    # @return [Hash] radicals with the user's stats.
+    def radicals_list(levels = nil)
+      return level_items_list("radicals", levels)
+    end
+
+    # Gets the user's full list of kanji and stats.
+    #
+    # @param levels [Integer, Array<Integer>] a specific level or array of
+    #   levels to fetch items for.
+    # @return [Hash] kanji with the user's stats.
+    def kanji_list(levels = nil)
+      return level_items_list("kanji", levels)
+    end
+
+    # Gets the user's full list of vocabulary and stats.
+    #
+    # @param levels [Integer, Array<Integer>] a specific level or array of
+    #   levels to fetch items for.
+    # @return [Hash] vocabulary with the user's stats.
+    def vocabulary_list(levels = nil)
+      return level_items_list("vocabulary", levels)
+    end
+
     # Gets the full response of the Level Progression API call.
     #
-    # @return [Hash] Full response from the Level Progression API call.
+    # @return [Hash] full response from the Level Progression API call.
     def full_level_progression_response
       return api_response("level-progression")
     end
 
     private
 
-    def method_missing(name, *args)
-      super unless [:radicals_list, :kanji_list, :vocabulary_list].include?(name)
-
-      levels = args.push
+    # Fetches the specified item type list from WaniKani's API
+    #
+    # @param type [String] The type of item to fetch.
+    # @param levels [Integer, Array<Integer>] a specific level or array of
+    #   levels to fetch items for.
+    # @return [Hash] list of items of the specified type and levels.
+    def level_items_list(type, levels)
       levels = levels.join(',') if levels.is_a?(Array)
-      response = api_response(name.to_s.gsub("_list", ""), levels)
+      response = api_response(type, levels)
 
       # The vocabulary API call without specifying levels returns a Hash instead
       # of an Array, so this is a hacky way of dealing with it.
